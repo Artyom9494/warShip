6d8dcf4bf3b3254da05ab7d8b08a2702e630a2a6
Artyom Kalinkin <artemklankin@yandex.ru>
2022-09-12 12:13:45 +0300

    leson java

---
+++ src/While.java
@@ -0,0 +1,13 @@
+public class While {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 11) {
+            if (a < 11) {
+                System.out.println( a + " сейчас длобавлю плюс один.");
+                a++;
+            } else {
+                System.out.println("the end");
+            }
+        }
+    }
+}
